2068239215b5ff146ac273ffa585b345b33432f8
MDietermann <mdietermann253@gmail.com>
2025-06-11 09:24:22 +0200

    Add CORS support for Flask application to enable cross-origin requests
    Signed-off-by: MDietermann <mdietermann253@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 ﻿import os
 
 from flask import Flask
-
 from API import spotify_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
+app.config.from_object(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app, resources={r'/*': {
+    'origins':
+        'http://localhost:8080',
+    'allow_headers':
+        'Access-Control-Allow-Origin'
+}})
 
 app.register_blueprint(spotify_bp)
 
